95bfdeaa2fd6672b6b603d29089897a232daacef
jacob <jsryan2@buffalo.edu>
2025-02-02 20:30:16 -0500

    Utilities for model results

--- training_utils.py
+++ training_utils.py
@@ -1,5 +1,9 @@
 from torch.utils.data import DataLoader, Subset
 from sklearn.model_selection import KFold
+from sklearn.metrics import f1_score, accuracy_score, precision_recall_fscore_support, confusion_matrix
+import torch
+import torch.nn as nn
+
 
 def kfold(model, train_ds, train_func, test_func, optimizer, device, epochs, batch_size, k=4):
     result = 0
@@ -16,3 +20,74 @@ def kfold(model, train_ds, train_func, test_func, optimizer, device, epochs, bat
         _ = train_func(model, train_dl, optimizer, device, epochs)
         result += test_func(model, validate_subset, device)
     return result / k
+
+
+def f1score(model, data, device):
+    x, y = next(iter(DataLoader(data, batch_size=len(data))))
+    y = y.argmax(1)
+    model.eval()
+    with torch.no_grad():
+        x = x.to(device)
+        y_hat = model(x).argmax(1)
+    return f1_score(y.cpu(), y_hat.cpu(), average='macro')
+
+
+def accuracy(model, data, device):
+    x, y = next(iter(DataLoader(data, batch_size=len(data))))
+    y = y.argmax(1)
+    model.eval()
+    with torch.no_grad():
+        x = x.to(device)
+        y_hat = model(x).argmax(1)
+    return accuracy_score(y.cpu(), y_hat.cpu())
+
+
+def prfs(model, data, device):
+    x, y = next(iter(DataLoader(data, batch_size=len(data))))
+    y = y.argmax(1)
+    model.eval()
+    with torch.no_grad():
+        x = x.to(device)
+        y_hat = model(x).argmax(1)
+    return precision_recall_fscore_support(y.cpu(), y_hat.cpu(), average='macro')
+
+
+def cm(model, data, device):
+    x, y = next(iter(DataLoader(data, batch_size=len(data))))
+    y = y.argmax(1)
+    model.eval()
+    with torch.no_grad():
+        x = x.to(device)
+        y_hat = model(x).argmax(1)
+    return confusion_matrix(y.cpu(), y_hat.cpu())
+
+
+def test_cnn(model, test_dl, device):
+    model.eval()
+    test_loss = 0
+    dataset_length = len(test_dl.dataset)
+    loss_fn = nn.CrossEntropyLoss()
+    with torch.no_grad():
+        for batch in test_dl:
+            x, y = batch[0].to(device), batch[1].to(device)
+
+            y_hat = model(x)
+            loss = loss_fn(y_hat, y)
+
+            test_loss += loss.item()
+    return test_loss / dataset_length
+
+
+def aggregated_metrics(model, data, device, window=5):
+    predictions, labels = [], []
+
+    unique_labels = set(label.argmax(0).item() for _, label in data)
+    for label in unique_labels:
+        subset = [x[0] for x in data if x[1].argmax(0).item() == label]
+        for i in range(0, len(subset), window):
+            batch = torch.stack(subset[i: i + window]).to(device)
+            predictions.append(torch.mode(model(batch).argmax(1))[0].item())
+            labels.append(label)
+    acc = accuracy_score(labels, predictions)
+    precision, recall, fscore, _ = precision_recall_fscore_support(labels, predictions, average='macro')
+    return acc, precision, recall, fscore
